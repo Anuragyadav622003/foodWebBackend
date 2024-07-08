fd9e60cb25d08b115c1e4b9dd152621d23050718
Anurag yadav <anuragyadav622003@gmail.com>
2024-07-08 15:09:41 +0530

    index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ connectDB();
 
 //api endpoints
 app.use("/food",foodRouter)
-app.use("/images",express.static('uploads'))
+// app.use("/images",express.static('uploads'))
 app.use("/User_register", register);
 app.use("/User_login", login);
 app.use("/getUserProfile", verifyToken, getUserProfile);
